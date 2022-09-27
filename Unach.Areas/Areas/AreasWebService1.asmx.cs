c56463e70270fbc7d023e15d6292e2b3d8a38201
carlos emmanuel aguilar mijangos <ch.arly98@hotmail.com>
2022-09-26 23:55:21 -0500

    Se ingreso las areas

--- Unach.Areas/Areas/AreasWebService1.asmx.cs
+++ Unach.Areas/Areas/AreasWebService1.asmx.cs
@@ -22,5 +22,23 @@ namespace Areas
         {
             return "Hola a todos";
         }
+        [WebMethod]
+        public decimal Cuadrado(decimal l1, decimal l2)
+        {
+            Calcular c = new Calcular();
+            return c.Cuadrado(l1, l2);
+        }
+        [WebMethod]
+        public decimal Triangulo(decimal b, decimal h)
+        {
+            Calcular c = new Calcular();
+            return c.Triangulo(b, h);
+        }
+        [WebMethod]
+        public double Circulo(double r)
+        {
+            Calcular c = new Calcular();
+            return c.Circulo(r);
+        }
     }
 }
--- Unach.Areas/Areas/Calcular.cs
+++ Unach.Areas/Areas/Calcular.cs
@@ -7,5 +7,22 @@ namespace Areas
 {
     public class Calcular
     {
+        
+        public decimal Cuadrado(decimal l1, decimal l2)
+        {
+            return l1 * l2;
+        }
+        public decimal Triangulo(decimal b, decimal h)
+        {
+            var Area = b * h / 2;
+            return Area;
+        }
+        public double Circulo(double radio)
+        {
+            double Radio = radio;
+            double area;
+            area = (Radio * radio) * Math.PI;
+            return area;
+        }
     }
 }
